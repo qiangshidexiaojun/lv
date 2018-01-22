c2b374752981d661ab16dce2951c9a71871e1e13
qiangshidexiaojun <1254520233@qq.com>
2018-01-22 11:05:41 +0800

    旅游静态页面完成

---
+++ js/index.js
@@ -0,0 +1,8 @@
+window.onload = function(){
+    var mySwiper = new Swiper('.swiper-container', {
+        autoplay: 1000,
+        loop: true,
+        // 如果需要分页器
+        pagination: '.swiper-pagination',
+    })  
+}
